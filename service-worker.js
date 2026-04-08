1c4e1d2c749a2290fbd727c264663a91c610d524
khennete.com <109117867+khennete-com@users.noreply.github.com>
2026-04-08 21:45:17 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = "app-cache-v2";
-const urlsToCache = [
+const CACHE_NAME = "costingv4";
+
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
@@ -7,20 +8,39 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
-// Install
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+// install
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-// Fetch (offline support)
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+// fetch
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
   );
+});
+
+// cleanup old cache
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("message", event => {
+  if (event.data && event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
 });
